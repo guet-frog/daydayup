54db8776b358bed15f5cab72d0322257f227b6db
clouder-frog <34978643+clouder-frog@users.noreply.github.com>
2018-07-17 17:03:50 +0800

    add C note for array_name

--- linux2win/c_test/array_name.c
+++ linux2win/c_test/array_name.c
@@ -1,61 +1,64 @@
-
-
-
 #include <stdio.h>
 
 int main(void)
 {
-	int a[7] = {1, 2, 3, 4, 5, 6, 7};
-	int *t = a;
-	int n = 0;
+	char a[5] = {0};
+	char b[2][5] = {0};
 	
-	for (n = 0; n<7; ++n)
-	{
-		*t++ = 999;
-		printf("a = %p\n", a);
-		printf("t = %p\n", t);
-		
-		// t++;
-		
-		// printf("t[%d] = %d\n", n, t[n]);
-	}
+	printf("a   = %p\n", a);
+	printf("a+1 = %p\n", a+1);	// 1
 	
-	// printf("t[5] = %d\n", t[5]);
-	// printf("t[6] = %d\n", t[6]);
+	printf("&a  = %p\n", &a);
+	printf("&a+1= %p\n", &a+1);	// 5
 	
+	printf("b   = %p\n", b);
+	printf("b+1 = %p\n", b+1);	// warning: 5
 	
-	return 0;
-}
-
-
-
-
-/*
-int main(void)
-{
-	char *a[4] = {0};
-	int  *b[4] = {0};
-
-	char **ptr = null;
-	char **ptr_1 = NULL;
+	printf("&b  = %p\n", &b);
+	printf("&b+1= %p\n", &b+1);	// 0xA
 	
-	printf("a    = %p\n", 		a);	
-	printf("a+1  = %p\n", 		a+1);
-	printf("&a   = %p\n", 		&a);
-	printf("&a+1 = %p\n\n",	 	&a+1);
+	printf("\n\n");
 	
-	printf("b    = %p\n", 		b);
-	printf("b+1  = %p\n", 		b+1);
-	printf("&b   = %p\n",		&b);
-	printf("&b+1 = %p\n\n",		&b+1);
+	printf("b[0]   		= %p\n", b[0]);
+	printf("b[0]+1   	= %p\n", b[0]+1);	// warning
 	
-	printf("ptr   = %p\n", 		ptr);
-	printf("ptr_1 = %p\n\n", 	ptr_1);
+	printf("&b[0]   	= %p\n", &b[0]);
+	printf("&b[0]+1   	= %p\n", &b[0]+1);	// warning
+	
+	printf("&b[0][0]   	= %p\n", &b[0][0]);
+	printf("&b[0][0]+1 	= %p\n", &b[0][0]+1);	// 1
 	
 	return 0;
 }
 
-// a 	 = 0xbfccd6a8
-// b 	 = 0xbfccd698
-*/
+a   = 0xbfa9e847
+a+1 = 0xbfa9e848
+&a  = 0xbfa9e847
+&a+1= 0xbfa9e84c
+
+&b  = 0xbfa9e83d	// 二维数组指针类型
+&b+1= 0xbfa9e847
+
+b   = 0xbfa9e83d	// warning: 二维数组名 -- 一维数组指针类型(非普通类型)
+b+1 = 0xbfa9e842
+
+b[0]   		= 0xbfa9e83d	// 等效为 -- 一维数组名(普通类型)
+b[0]+1   	= 0xbfa9e83e
+
+&b[0]   	= 0xbfa9e83d	// 等效为 -- 一维数组名取地址(非普通类型)
+&b[0]+1   	= 0xbfa9e842
+
+&b[0][0]   	= 0xbfa9e83d	// 普通类型
+&b[0][0]+1 	= 0xbfa9e83e
+
+
+
+
+
+
+
+
+
+
+
 
