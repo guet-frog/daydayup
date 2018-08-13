960eff567c2d214f29614df9a83dcc0ad6ea9446
frog <qing.sun@cloudersemi.com>
2018-08-13 22:22:03 +0800

    整理github内容

--- uboot/0.read_note.c
+++
@@ -1,5 +0,0 @@
-
-
-bic	r1, r1, r2
-
-https://www.cnblogs.com/huhu0013/p/4103024.html
--- uboot/ubootSrc/u-boot-new/go.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-#mips-linux-gnu-objdump -D -S u-boot > u-boot.s
-#cp u-boot.s /home/huxiaoan/Downloads/clouder800/u-boot-2014.s -rf
-#cp u-boot.bin /home/huxiaoan/Downloads/clouder800/u-boot-2014.bin -rf
