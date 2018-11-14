e2b12f544d2c6cd2803bea07972255ed0715484d
frog@home <sq1200840233@163.com>
2018-11-14 09:43:38 +0800

    添加uboot_official相关源码到linux2win

---
+++ linux2win/uboot_official/copy.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+	cp ~/src/uboot_official/u-boot.bin /mnt/hgfs/linux2win/uboot_official/fastboot/uboot.bin
+	
+	cp ~/src/uboot_official/u-boot.bin /mnt/hgfs/linux2win/uboot_official/sd_fusing/u-boot.bin
