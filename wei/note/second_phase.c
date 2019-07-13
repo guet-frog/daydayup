d10e10b94a87fc6dc43d0d72b4c10df6ad545fe7
frog <qing.sun@cloudersemi.com>
2019-07-13 11:03:26 +0800

    source insight black_background.xml

--- wei/note/second_phase.c
+++ wei/note/second_phase.c
@@ -1,27 +1,28 @@
+
 #if	/* 12.1 字符设备驱动程序概念 */
 
 	// 6′
-	应用程序在调用open()，read()等时，实际通过执行swi指令(swi xxx)，这条汇编
-	指令会引发一次异常，进入系统异常处理函数中(进入内核中)，在异常处理函数中
-	通过判断xxx调用不同的处理函数。
+	(1) 应用程序在调用open()，read()等时，实际通过执行swi指令(swi xxx)，这条汇编
+		指令会引发一次异常，进入系统异常处理函数中(进入内核中)，在异常处理函数中
+		通过判断xxx调用不同的处理函数。
 
-	根据打开的文件类型(文件属性、主设备号)来调用相应的open、read函数。
+	(2) 根据打开的文件类型(文件属性、主设备号)来调用相应的open、read函数。
 #endif
 
 #if /* 12.2.1 字符设备驱动led */
 
 	定义file_operation如何通知内核 -- register_chrdev()
 	register_chrdev()被谁调用(驱动的入口函数是哪一个) -- module_init()
-	
+
 	字符设备驱动通过数组来管理，major为下标
-	
+
 	自动创建通过 应用程序的udev机制(mdev)
 	利用系统信息(注册驱动时会在/sys目录下生成设备信息)，mdev会根据系统信息自
 	动创建设备节点
-
+	
 	dev_class = class_create();		// 创建类
 	dev_device = class_device_create();		// 类下创建设备
-	
+
 	/etc/init.d/rcS
 	echo /sbin/mdev > /proc/sys/kernel/hotplug
 #endif
